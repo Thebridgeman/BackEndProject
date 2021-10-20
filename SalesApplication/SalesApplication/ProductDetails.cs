705a6c5ebbcca65ae5be33d4384af2a3c88ea7c0
Thebridgeman <will.bridgeman@yahoo.co.uk>
2021-10-20 13:00:47 +0100

    first commit to dev

---
+++ SalesApplication/SalesApplication/ProductDetails.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalesApplication
+{
+    class ProductDetails
+    {
+        public void CreateItem()
+        {
+            //Console.WriteLine(" What is the Sale ID");
+            //string ID= Console.ReadLine();
+
+            Console.WriteLine("Product Name");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Sale Quantity");
+            int quantity = Int32.Parse(Console.ReadLine());
+
+
+            Console.WriteLine("Individual Item Price");
+            float price = float.Parse (Console.ReadLine());
+
+            DateTime SaleDate;
+            Console.WriteLine("Sale Date format MM/DD/YYYY");
+            SaleDate = DateTime.Parse(Console.ReadLine());
+            
+
+            
+        }
+
+    }
+}
---
+++ SalesApplication/SalesApplication/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace SalesApplication
+{
+    class Program
+    {
+        public enum CrudOptions
+
+        {
+            CREATE,
+            READ,
+            UPDATE,
+            DELETE,
+            QUIT
+        }
+        static void Main(string[] args)
+        {
+            string s = " Sales ";
+            Console.SetCursorPosition((Console.WindowWidth - s.Length) / 2, Console.CursorTop);
+            Console.WriteLine(s);
+
+            Console.WriteLine(" ===== Menu ===== ");
+            Console.WriteLine("1. Make an Entry");
+            Console.WriteLine("2. Sales Reports");
+            Console.WriteLine(" === ======= === ");
+
+       
+            string input = Console.ReadLine();
+
+            if(input == "1") {
+                // open CRUD menu
+
+                Console.WriteLine(" 1. Create ");
+                Console.WriteLine(" 2. Update ");
+                Console.WriteLine(" 3. Delete ");
+
+                string input2 = Console.ReadLine();
+
+                switch (input2)
+                {
+                    case "1":
+                        // create 
+                        break;
+
+                    case "2":
+                        // Update 
+                        break;
+
+                    case "3":
+                        // delete
+                        break;
+
+                }
+            }
+            else if(input == "2")
+            {
+                // view reports
+            }
+            else
+            {
+                Console.WriteLine("Invalid Input");
+            }
+        }
+
+        public static void Create()
+        {
+            // get input from user and store them
+        }
+
+    }
+    
+
+}
